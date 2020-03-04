7d0a5fc43e49266e591b18ab9566ada9587b8743
Peejay15 <pjruggirello@gmail.com>
2020-03-04 14:43:54 -0500

    Added the FindMyCar and the MyFriends pages

---
+++ CarFinder/MyFriendsEntryPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CarFinder
+{
+    public partial class MyFriendsEntryPage : ContentPage
+    {
+        public MyFriendsEntryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

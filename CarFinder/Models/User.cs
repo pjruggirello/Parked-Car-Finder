40b3610481ad6eeb0748f9eeb0924e9eb6b8c018
Peejay15 <pjruggirello@gmail.com>
2020-04-09 11:26:53 -0400

    Added.

---
+++ CarFinder/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFinder.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User() { }
+        public User(string Username, string Password)
+        {
+            this.Username = Username;
+            this.Password = Password;
+        }
+
+        public bool CheckInformation()
+        {
+            if(!this.Username.Equals("") && !this.Password.Equals(""))
+                return true;
+            else
+                return false;
+        }
+    
+    }
+}

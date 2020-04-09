5e7ea68cf2b20ba28483db3bd49d9f046d3ace63
Peejay15 <pjruggirello@gmail.com>
2020-04-09 12:59:58 -0400

    Should work now

---
+++ CarFinder/Views/MyFriendsEntryPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CarFinder.Views
+{
+    public partial class MyFriendsEntryPage : ContentPage
+    {
+        public MyFriendsEntryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

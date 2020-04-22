04369dd3f6331c58b1c4c9742a6a8986930fc9e0
Jack Shashaty <john.shashaty@student.fairfield.edu>
2020-04-22 14:44:50 -0400

    android icon set

--- CarFinder.Android/MainActivity.cs
+++ CarFinder.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Android.Runtime;
 
 namespace CarFinder.Droid
 {
-    [Activity(Label = "CarFinder", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "CarFinder", Icon = "@drawable/carFinder", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
